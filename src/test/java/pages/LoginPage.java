6bc11d89c161e60ce237b55c9e2a086a3748b6aa
ngangwal <gangwaln@gmail.com>
2022-11-06 15:53:06 +0530

    pages

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -2,6 +2,8 @@ package pages;
 
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends BasePage{
 
@@ -10,6 +12,54 @@ public class LoginPage extends BasePage{
         super(driver);
     }
 
+    @FindBy(id = "input-email")
+    WebElement email;
+
+    @FindBy(id = "input-password")
+    WebElement paswd;
+
+    @FindBy(linkText = "Forgotten Password")
+    WebElement ForgotPwdLink;
+
+//    @FindBy(className = "btn btn-primary")
+//    WebElement loginbtn;
+
+    @FindBy(xpath = "//a[text() = 'Continue']")
+    WebElement continuebtn;
+
+    @FindBy(xpath = "//input[@type = 'submit' and @value='Login']")
+    WebElement loginbtn;
+
+    @FindBy(linkText = "Address Book")
+    WebElement AddressBook;
+
+    @FindBy(linkText = "Wish List")
+    WebElement WishList;
+
+    @FindBy(linkText = "Order History")
+    WebElement OrderHistory;
+
+    @FindBy(linkText = "Downloads")
+    WebElement Downloads;
+
+    @FindBy(linkText = "Recurring payments")
+    WebElement Recurringpayments;
+
+    @FindBy(linkText = "Reward Points")
+    WebElement RewardPoints;
+
+    @FindBy(linkText = "Returns")
+    WebElement Returns;
+
+    @FindBy(linkText = "Transactions")
+    WebElement Transactions;
+
+    @FindBy(linkText = "Newsletter")
+    WebElement NewLetter;
+
+
+
+    //input[@type = 'submit' and @value='Login']
 
     // Object Repository
 
--- src/test/java/pages/MenuPage.java
+++ src/test/java/pages/MenuPage.java
@@ -2,6 +2,8 @@ package pages;
 
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class MenuPage extends BasePage{
 
@@ -9,6 +11,9 @@ public class MenuPage extends BasePage{
     public MenuPage(WebDriver driver) {
         super(driver);
     }
+
+    @FindBy(xpath = "//ul[@class= 'nav navbar-nav']/li[1]/a[text() ='Desktops' ]")
+    WebElement menuDesktop;
 
 
     // Object Repository
